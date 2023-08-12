fe73756608c1d223f8a3bf410b957f003361d1ff
yassinerahhou <yassine.rahhou.1@gmail.com>
2023-08-12 15:00:13 -0700

    By yacin

--- console.py
+++ console.py
@@ -1,13 +1,21 @@
 import cmd
+
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
-    def do_quit(self , arg) :
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
         return True
+
     def do_EOF(self, arg):
+        """Exit the program with Ctrl+D (EOF)"""
+        print()  # Print a newline
         return True
+
     def emptyline(self):
         """Do nothing when an empty line is entered."""
-        pass    
+        pass
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
 
