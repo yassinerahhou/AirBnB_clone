1a97fbd19901e7b88bfb84a5c63b899afa578153
ZAKARIA <zakariaelaro@gmail.com>
2023-08-12 08:12:31 -0700

    Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -1,40 +1,50 @@
-import uuid
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
 from datetime import datetime
+
+
 class BaseModel:
-    """ clase baseModel
-    """
-    name = str
-    my_number =     int
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        
-    def __str__(self):
+    """Represents the BaseModel"""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize BaseModel.
+
+        Args:
+            *args(any): Unused.
+            **kwargs (dict)
         """
-        str fonction
-        """
-        self.__dict__= {
-        "my number" : self.my_number,
-        "name":self.name,
-        "__class__":self.__class__.__name__,
-        "updated_at":self.updated_at,
-        "id":self.id,
-        "created_at":self.created_at,
-        }
-        return f"[{type(self).__name__}] {self.id} {self.__dict__}"
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
     def save(self):
-        """ save fonction"""
-        self.updated_at = datetime.now()
+        """Update updated_at with datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
+
     def to_dict(self):
-        """ to_dict fonction"""
-        mydictionair = {
-            "my number" : self.my_number,
-            "name":self.name,
-            "__class__":self.__class__.__name__,
-            "updated_at":self.updated_at.isoformat(),
-            "id":self.id,
-            "created_at":self.created_at.isoformat(),
-           
-            }
-        return mydictionair
+        """Return the dictionary
+
+        Includes the key/value pair
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str"""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
